6690c20bb015881e6e014ad2f00ef0c93ba25da6
krahil1996 <rahil393@gmail.com>
2025-01-24 18:35:17 +0530

    update files

--- EcommApp-Task-2/script.js
+++ EcommApp-Task-2/script.js
@@ -1,10 +1,14 @@
 const menuLine = document.getElementById('menu-line');
 const menuItems = document.getElementById('menu-items');
+const menuItemsBtn = document.getElementsByTagName('button');
 const menubtnIcon = document.querySelector('i')
 menuLine.addEventListener('click', () => {
     menuItems.classList.toggle('open');
     const isOpen = menuItems.classList.contains('open');
     menubtnIcon.setAttribute('class', isOpen ? 'bx bx-x-circle' : 'bx bx-menu');
     console.log(isOpen);
-
 })
+
+const menuClose = () => {
+    menuItems.classList.remove('open');
+}
